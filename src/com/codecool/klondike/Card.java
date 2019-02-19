bc13f87764caf7b157ea7f4ba77758f5153668a8
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-02-19 14:09:19 +0100

    variable containing deck renamed from 'result' to 'deck'

--- src/com/codecool/klondike/Card.java
+++ src/com/codecool/klondike/Card.java
@@ -87,13 +87,13 @@ public class Card extends ImageView {
     }
 
     public static List<Card> createNewDeck() {
-        List<Card> result = new ArrayList<>();
+        List<Card> deck = new ArrayList<>();
         for (int suit = 1; suit < 5; suit++) {
             for (int rank = 1; rank < 14; rank++) {
-                result.add(new Card(suit, rank, true));
+                deck.add(new Card(suit, rank, true));
             }
         }
-        return result;
+        return deck;
     }
 
     public static void loadCardImages() {
